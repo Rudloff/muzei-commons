6a7c26eaa2ec63c7de40446cf3f500eec5ed3f57
Pierre Rudloff <contact@rudloff.pro>
2019-03-23 20:01:39 +0100

    First WIP

---
+++ src/main/java/pro/rudloff/muzei/commons/CommonsService.kt
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package pro.rudloff.muzei.commons
+
+import okhttp3.OkHttpClient
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+import java.io.IOException
+
+internal interface CommonsService {
+
+    companion object {
+
+        private fun createService(): CommonsService {
+            val okHttpClient = OkHttpClient.Builder()
+                    .addInterceptor { chain ->
+                        var request = chain.request()
+                        val url = request.url().newBuilder()
+                                .addQueryParameter("client_id", CONSUMER_KEY).build()
+                        request = request.newBuilder().url(url).build()
+                        chain.proceed(request)
+                    }
+                    .build()
+
+            val retrofit = Retrofit.Builder()
+                    .baseUrl("https://commons.wikimedia.org/")
+                    .client(okHttpClient)
+                    .addConverterFactory(MoshiConverterFactory.create())
+                    .build()
+
+            return retrofit.create<CommonsService>(CommonsService::class.java)
+        }
+
+        @Throws(IOException::class)
+        internal fun todayQuery(): CommonsService.Response {
+            return createService().todayQuery.execute().body()
+                    ?: throw IOException("Response was null")
+        }
+
+        @Throws(IOException::class)
+        internal fun photoInfo(): CommonsService.Response {
+            return createService().photoInfo.execute().body()
+                    ?: throw IOException("Response was null")
+        }
+
+        @Throws(IOException::class)
+        internal fun trackDownload(url: String) {
+            createService().trackDownload(url).execute()
+        }
+    }
+
+    @get:GET("w/api.php?action=query&prop=images&titles=Template:Potd/2019-03-23&format=json&imlimit=1&formatversion=2")
+    val todayQuery: Call<Response>
+
+    @get:GET("w/api.php?action=query&prop=imageinfo&titles=File:Vents%20du%20Sud%20-%20Le%20Grau-du-Roi%2004.jpg&format=json&iiprop=url|user|canonicaltitle&formatversion=2")
+    val photoInfo: Call<Response>
+
+    @GET("{url}")
+    fun trackDownload(@Path("id") photoId: String): Call<Any>
+
+    data class Response(
+        val query: Query
+    )
+
+    data class Query(
+        val pages: List<Page>
+    )
+
+    data class Page(
+        val images: List<Image>?,
+        val imageinfo: List<Imageinfo>?
+    )
+
+    data class Image(
+        val title: String
+    )
+
+    data class Imageinfo(
+        val url: String,
+        val user: String,
+        val descriptionurl: String,
+        val canonicaltitle: String
+    )
+}
---
+++ src/main/java/pro/rudloff/muzei/commons/CommonsWorker.kt
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package pro.rudloff.muzei.commons
+
+import android.content.Context
+import android.util.Log
+import androidx.core.net.toUri
+import androidx.work.Constraints
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import pro.rudloff.muzei.commons.BuildConfig.UNSPLASH_AUTHORITY
+import com.google.android.apps.muzei.api.provider.Artwork
+import com.google.android.apps.muzei.api.provider.ProviderContract
+import java.io.IOException
+
+class CommonsWorker(
+    context: Context,
+    workerParams: WorkerParameters
+) : Worker(context, workerParams) {
+
+    companion object {
+        private const val TAG = "UnsplashExample"
+
+        internal fun enqueueLoad() {
+            val workManager = WorkManager.getInstance()
+            workManager.enqueue(OneTimeWorkRequestBuilder<CommonsWorker>()
+                    .setConstraints(Constraints.Builder()
+                            .setRequiredNetworkType(NetworkType.CONNECTED)
+                            .build())
+                    .build())
+        }
+    }
+
+    override fun doWork(): Result {
+        val images = try {
+            CommonsService.todayQuery().query.pages[0].images
+        } catch (e: IOException) {
+            Log.w(TAG, "Error reading Unsplash response", e)
+            return Result.retry()
+        }
+
+        if (images!!.isEmpty()) {
+            Log.w(TAG, "No photos returned from API.")
+            return Result.failure()
+        }
+
+        val photo = try {
+            CommonsService.photoInfo().query.pages[0].imageinfo?.get(0)
+        } catch (e: IOException) {
+            Log.w(TAG, "Error reading Unsplash response", e)
+            return Result.retry()
+        }
+
+        val providerClient = ProviderContract.getProviderClient(
+                applicationContext, UNSPLASH_AUTHORITY)
+        val attributionString = applicationContext.getString(R.string.attribution)
+        providerClient.addArtwork(
+            Artwork().apply {
+                // token = photo.id
+                title = photo?.canonicaltitle
+                byline = photo?.user
+                attribution = "Wikimedia Commons"
+                persistentUri = photo?.url?.toUri()
+                webUri = photo?.descriptionurl?.toUri()
+            }
+        )
+        return Result.success()
+    }
+}
